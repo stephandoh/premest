72cb70548552cd032668d8c079aca4e6c47b4a44
Stephanie Dawsonn-Andoh <stephandoh@gmail.com>
2020-11-16 17:07:24 +0000

    added form

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Navbar from './Navbar'
+import Button from './Button'
+import Form from './Form'
 function App() {
   return (
     <div className="app">
-      <h1>Hey, I am learning react</h1>
-      <Navbar />
-    </div>
-    
+      <Button />
+      <Form />
+    </div>    
   );
 }
 
